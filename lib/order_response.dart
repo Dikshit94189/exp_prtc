907462f77cd17b9bfd93ba99e54499e271d56548
Dikshit Sharma <noreply@feedback.github.com>
2026-09-13 22:30:03 +0530

    add static json

--- lib/order_response.dart
+++ lib/order_response.dart
@@ -1,13 +1,15 @@
+import 'dart:convert';
+
 class OrderResponse {
   final List<OrderResponse> orderRes;
 
   OrderResponse({required this.orderRes});
 
-  factory OrderResponse.fromJson(Map<String, dynamic> json) {
-    return OrderResponse(
-      orderRes: (json["orders"] as List)
-          .map((order) => OrderResponse.fromJson(order))
-          .toList(),
-    );
-  }
+   factory OrderResponse.fromJson(Map<String , dynamic> json){
+     return OrderResponse(
+       orderRes: (json['orders'] as List).
+         map((order) => OrderResponse.fromJson(order))
+           .toList(),
+     );
+   }
 }
