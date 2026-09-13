d4a39c4a6668e2b1fa1d0e03c4e7884b58a61c1f
Dikshit Sharma <noreply@feedback.github.com>
2026-09-13 21:45:38 +0530

    order_data

---
+++ lib/Order.dart
@@ -0,0 +1,16 @@
+class OrderModel {
+  final int id;
+  final String product;
+  final dynamic price;
+
+  OrderModel({required this.id, required this.product, required this.price});
+
+  factory OrderModel.fromJson(Map<String, dynamic> json) {
+    return OrderModel(
+      id: json['id'],
+      product: json['product'],
+      price: (json['price'] as num).toDouble(),
+    );
+  }
+
+}
