16193b25979beadae66f01908527818565b98d7d
Dikshit Sharma <noreply@feedback.github.com>
2026-09-13 23:28:21 +0530

    add flutter main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'order_view.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(),
+      home: const OrderView(),
     );
   }
 }
